6c8f28d5d274301088249e31e9cb325f8dc7d202
RTOSkit <rtoskit@gmail.com>
2015-02-22 23:06:18 +0100

    update functions code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,58 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
 
+# this function generate a custom class that return 
+# an object type list with all properties and functions 
+# that's needed for make cacheable, the invertible matrix result
+# @param: x = user matrix based on
+# @return: list mixed object
 makeCacheMatrix <- function(x = matrix()) {
-
+  IM <- NULL
+  set <- function(y) {
+    x <<- y
+    IM <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solve) IM <<- solve
+  getinv <- function() IM
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
 ## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+# this function process the user matrix inversion, 
+# if the matrix object, have a cached invert result, 
+# the function return this cached result, 
+# but if the object not has cached result, 
+# a new inverted matrix is returned.
+# @param:  x = user matrix based on
+#        ... = super class params
+# @return: inverse of the input matrix
+cacheSolve <- function(x, ...) {   
+  IM <- x$getinv()
+  if(!is.null(IM)) {
+    #message("getting cached data")
+    return(IM)
+  }
+  data <- x$get()
+  IM <- solve(data, ...)
+  x$setinv(IM)
+  IM
+  ## Return a matrix that is the inverse of 'x'
 }
+
+
+### DEMO IMPLEMENTATION
+# define the size of the square matrix
+#// SQUARE_SIZE <- 3
+# make an "ones" matrix
+#// A <- matrix(1,SQUARE_SIZE,SQUARE_SIZE)
+# make invertible "A" matrix
+#// A <- A + diag(SQUARE_SIZE)
+# make cacheable matrix
+#// CA <- makeCacheMatrix(A)
+# first solve intent
+#// cacheSolve(CA)
+# second solve intent
+#// cacheSolve(CA)
